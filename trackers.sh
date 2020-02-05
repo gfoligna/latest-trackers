78e10b36da89143fc156819e81a31aaac5db5944
Gonzalo Foligna <gfoligna@gmail.com>
2020-02-05 15:06:25 -0300

    Adding variable

--- trackers.sh
+++ trackers.sh
@@ -4,12 +4,17 @@
 
 #set -e
 out_dir=$1
+filename=$2
 
 if [ $1 -e ]; then
   out_dir='.'
 fi
 
-curl -s https://torrents.me/tracker-list/?download=latest > ${out_dir}/trackers_`date +%Y%m%d-%H%M`
+if [ $2 -e ]; then
+  filename="trackers_`date +%Y%m%d-%H%M`"
+fi
+
+curl -s https://torrents.me/tracker-list/?download=latest > ${out_dir}/${filename}
 if [ $? = 0 ]; then
   echo 'File created with latest trackers'
 else
