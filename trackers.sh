e8a274b227dc1f91bd50dc0ccb8f82262baa5bc4
Gonzalo Foligna <gfoligna@gmail.com>
2020-02-05 15:45:44 -0300

    Bad bracketing

--- trackers.sh
+++ trackers.sh
@@ -6,16 +6,16 @@
 out_dir=$1
 filename=$2
 
-if [ $1 -e ]; then
+if [[ $1 -e ]]; then
   out_dir='.'
 fi
 
-if [ $2 -e ]; then
+if [[ $2 -e ]]; then
   filename="trackers_`date +%Y%m%d-%H%M`"
 fi
 
 curl -s https://torrents.me/tracker-list/?download=latest > ${out_dir}/${filename}
-if [ $? = 0 ]; then
+if [[ $? = 0 ]]; then
   echo 'File created with latest trackers'
 else
   echo 'Please check errors with curl'
