c6c9917bff43db31d152972f3caf3b3db6e7f078
Gonzalo Foligna <gfoligna@gmail.com>
2021-01-03 20:31:28 -0300

    Initial commit after cleanup

---
+++ trackers.sh
@@ -0,0 +1,22 @@
+## Created by Gonzalo Foligna (gfoligna@gmail.com) ##
+
+#!/bin/bash
+
+#set -e
+out_dir=$1
+filename=$2
+
+if [[ -z $1 ]]; then
+  out_dir='.'
+fi
+
+if [[ -z $2 ]]; then
+  filename="trackers_`date +%Y%m%d-%H%M`"
+fi
+
+curl -s -L https://torrends.to/torrent-tracker-list/?download=latest > ${out_dir}/${filename}
+if [[ $? = 0 ]]; then
+  echo 'File created with latest trackers'
+else
+  echo 'Please check errors with curl'
+fi
